be130067ef07863448bbcd19a5d9930e9a78fd45
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-06-23 11:15:23 -0300

    Created init all components, styled Header and themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,5 @@
-import { gql, useQuery } from '@apollo/client';
+import { Event } from './pages/Event';
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
+export function App() {
+  return <Event />;
 }
-
-function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
-  return (
-    <ul>
-      {data?.lessons.map((lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>;
-      })}
-    </ul>
-  );
-}
-
-export default App;
