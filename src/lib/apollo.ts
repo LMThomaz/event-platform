edd586f9f38e835a4d3f8621e099e6c214aae3d2
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-07-15 15:33:46 -0300

    configure api apollo with token and url by ENV

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4q6w75s37ql01xl2qllazbv/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
